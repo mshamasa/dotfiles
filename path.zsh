788ca128159c5702cef0188172b85673cf34e194
mshamasa <mshamasa@gmail.com>
2022-10-19 09:29:14 -0700

    load global installed libraries

--- path.zsh
+++ path.zsh
@@ -4,6 +4,9 @@
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
+# Load global installed binaries
+export PATH="$HOME/node_modules/.bin:$PATH"
+
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:$PATH"
 
